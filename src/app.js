88236be79984f608c903c03fb66fa49f1e437c9c
Strahinja Ajvaz <strahinja.ajvaz@gmail.com>
2018-01-08 23:19:24 +1100

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import "./styles/styles.scss";
+import "normalize.css/normalize.css";
+
+ReactDOM.render(<p>This is my boiler plate</p>, document.querySelector("#app"));
